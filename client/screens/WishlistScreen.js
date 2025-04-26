4597bdae51ceb4ff7ae26b0a7a9aa0d11cd99480
Amine rahali <aminerahali2001@hotmail.com>
2025-04-26 22:55:01 +0100

    navbar

---
+++ client/screens/WishlistScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function WishlistScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Wishlist Screen</Text>
+    </View>
+  );
+}
