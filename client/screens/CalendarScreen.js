3c2228aa9a332d3a0830f0ab251bf3c378efe6da
Amine rahali <aminerahali2001@hotmail.com>
2025-04-29 09:36:55 +0100

    test

--- client/screens/CalendarScreen.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function CalendarScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Calendar Screen</Text>
-    </View>
-  );
-}
