e8ab7b94ec3f20c1058875e139d9017b4179104e
brokenmugg <abelmajidmaamar@gmail.com>
2025-05-06 15:35:29 +0100

    build: add expo-secure-store and remove async-storage
    This commit introduces expo-secure-store for secure storage management and removes the deprecated @react-native-async-storage/async-storage dependency. The change ensures better security for sensitive data storage in the application.

--- client/context/AuthContext.js
+++ client/context/AuthContext.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { createContext, useEffect, useState } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 export const AuthContext = createContext();
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
